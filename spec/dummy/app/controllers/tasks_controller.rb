2e7781f5af42f08e8d3dd911074601e9afd3e4dc
kaspernj <k@spernj.org>
2016-02-21 18:09:14 +0100

    Remember params through monkey patch. Able to show default values given by the user himself instead of through params

--- spec/dummy/app/controllers/tasks_controller.rb
+++ spec/dummy/app/controllers/tasks_controller.rb
@@ -1,7 +1,14 @@
 class TasksController < ApplicationController
   def index
     @ransack_params = params[:q] || {}
+    set_default_arguments if params[:set_default_arguments]
     @ransack = Task.ransack(@ransack_params)
     @tasks = @ransack.result
+  end
+
+private
+
+  def set_default_arguments
+    @ransack_params[:name_cont] = "Test sample cont"
   end
 end

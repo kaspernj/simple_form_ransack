484bb69c6dfe25bf93a153d032a5a16f53e94a6e
kaspernj <kaspernj@gmail.com>
2016-05-31 19:18:37 +0200

    Added simple_search_fields_for

--- app/helpers/simple_form_ransack_helper.rb
+++ app/helpers/simple_form_ransack_helper.rb
@@ -24,4 +24,30 @@ module SimpleFormRansackHelper
       yield form_proxy
     end
   end
+
+  def simple_search_fields_for(ransack, *args)
+    if args.last.is_a?(Hash)
+      opts = args.pop
+    else
+      opts = {}
+    end
+
+    opts[:url] = request.original_fullpath unless opts[:url]
+    opts[:method] = "get" unless opts[:method]
+    args << opts
+
+    model_class = ransack.klass
+    sample_model = model_class.new
+    ransack_params = ransack.instance_variable_get(:@_registered_params)
+
+    simple_fields_for(sample_model, *args) do |form|
+      form_proxy = SimpleFormRansack::FormProxy.new(
+        ransack: ransack,
+        form: form,
+        params: ransack_params
+      )
+
+      yield form_proxy
+    end
+  end
 end

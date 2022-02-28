658eac71b625dae4f7c77c458187cb81e8ad2458
kaspernj <k@spernj.org>
2022-02-28 08:38:11 +0000

    Fixed Rails 7 support

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -4,7 +4,7 @@ require File.expand_path("boot", __dir__)
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "sprockets/railtie"
+require "sprockets/railtie" unless Gem.loaded_specs["rails"].version.to_s.start_with?("7.")
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
@@ -16,6 +16,11 @@ class Dummy::Application < Rails::Application
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
+  if Gem.loaded_specs["rails"].version.to_s.start_with?("7.")
+    config.load_defaults 7.0
+  else
+    config.load_defaults 6.0
+  end
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
